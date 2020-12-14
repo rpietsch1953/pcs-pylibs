e525785e4c5c9460dfd8a64643f8e3d6f1c60252
rpietsch1953 <r.pietsch@pcs-at.com>
2020-12-14 16:06:56 +0100

    orrect setup

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="pcs-pylibs",
+    name="pcs_pylibs",
     version="0.0.1",
     author="Rainer Pietsch",
     author_email="r.pietsch@pcs-at.com",
